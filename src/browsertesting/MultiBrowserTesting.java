afa714fe52828d308b5d5d4c4bba56fdbc11e601
Dhara85 <idharap07@gmail.com>
2023-11-14 00:56:45 +0000

    Initial commit

---
+++ src/browsertesting/MultiBrowserTesting.java
@@ -0,0 +1,44 @@
+package browsertesting;
+
+/**
+ * Project-5 - ProjectName : com-utimateqa
+ * BaseUrl = https://courses.ultimateqa.com/
+ * 1. Setup chrome browser.
+ * 2. Open URL.
+ * 3. Print the title of the page.
+ * 4. Print the current url.
+ * 5. Print the page source.
+ * 6. Click on ‘Sign In’ link
+ * 7. Print the current url
+ * 8. Enter the email to email field.
+ * 9. Enter the password to password field.
+ * 10. Click on Login Button.
+ * 11. Navigate to baseUrl.
+ * 12. Navigate forward to Homepage.
+ * 13. Navigate back to baseUrl.
+ * 14. Refresh the page.
+ * 15. Close the browser.
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowserTesting {
+    static String browser = "Chrome";
+    static String baseUrl = "http://the-internet.herokuapp.com/login";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+    }
+}
